c90ad5c6f44e2e77de083f8f6f84bc56f121b1c7
SimiVoid <szymon.trembeckI@onmail.com>
2022-10-24 21:47:09 +0200

    Add main.c with signal handler and thread managment

---
+++ src/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "reader.h"
+
+#define THREAD_COUNT 1
+
+bool sig_stop = false;
+
+void sig_handler(int sig);
+
+int start_thread(pthread_t* thread, void* (*func)(void*), int* ret);
+int wait_for_thread_stop(pthread_t* thread);
+
+int main(void) {
+    if(signal(SIGTERM, sig_handler) == SIG_ERR) {
+        return 1;
+    }
+
+    pthread_t* threads = (pthread_t*)malloc(sizeof(pthread_t) * THREAD_COUNT);
+
+    int ret = 0;
+
+    // Start reader thread
+    if(start_thread(&threads[0], (void* (*)(void*))reader_main, &ret) != 0) {
+        return 1;
+    }
+
+    while(!sig_stop) {
+        if(ret != 0) {
+            sig_stop = true;
+            break;
+        }
+
+        usleep(100);
+    }
+
+    for(int i = 0; i < THREAD_COUNT; ++i) {
+        wait_for_thread_stop(&threads[i]);
+    }
+
+    free(threads);
+
+    return ret;
+}
+
+void sig_handler(int sig) {
+    if(sig == SIGTERM) {
+        sig_stop = true;
+    }
+}
+
+int start_thread(pthread_t* thread, void* (*func)(void*), int* ret) {
+    if(pthread_create(thread, NULL, func, ret) != 0) {
+        sig_stop = true;
+        return 1;
+    }
+
+    return 0;
+}
+
+int wait_for_thread_stop(pthread_t* thread) {
+    if(pthread_join(*thread, NULL) != 0) {
+        return 1;
+    }
+
+    return 0;
+}
